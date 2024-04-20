bd273477c4a759cfaa6fcd3707796f5438ec338f
shreegowtham27 <gforgowtham8@gmail.com>
2024-04-21 02:31:18 +0530

    Vuln Line Added and Updated Port

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.sendFile("templates/index.html", { root: __dirname });
+});
+
+app.get('/search', (req, res) => {
+	  const query = req.query.q;
+	  res.send(`<p>Search results for: ${query}</p>`);
 });
 
 app.listen(PORT, HOST);
